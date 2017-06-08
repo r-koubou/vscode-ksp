3bbe53e217c3bb3001853c50a8f0cbddd6067e5a
R-Koubou <dev.r.koubou@gmail.com>
2017-06-09 04:00:38 +0900

    プリファレンスに設定機能を追加（パーサ機能）エラー検知時のハンドリング精度の強化。

---
+++ src/KSPConfigConstants.ts
@@ -0,0 +1,21 @@
+/* =========================================================================
+
+    KSPConfigConstants.ts
+    Copyright(c) R-Koubou
+
+    [License]
+    MIT
+
+   ======================================================================== */
+
+export const CONFIG_SECTION_NAME                = 'ksp';
+
+// Validate
+export const KEY_JAVA_LOCATION              = 'java.command';
+export const KEY_ENABLE_VARIDATE            = 'validate.enable';
+export const KEY_ENABLE_REALTIME_VARIDATE   = 'validate.realtime.enable';
+export const KEY_REALTIME_VARIDATE_DELAY    = 'validate.realtime.delay';
+
+export const DEFAULT_ENABLE_VARIDATE        = false;
+export const DEFAULT_REALTIME_VARIDATE      = false;
+export const DEFAULT_VARIDATE_DELAY         = 500;
--- src/KSPValidationProvider.ts
+++ src/KSPValidationProvider.ts
@@ -12,20 +12,29 @@
 
 import * as vscode          from 'vscode';
 import * as cp              from 'child_process';
+import * as tmp             from 'tmp';
+import * as fs              from 'fs';
 import { ThrottledDelayer } from './libs/async';
+import * as config          from './KSPConfigConstants';
 
-
-const CheckedExecutablePath = 'ksp.validate.checkedExecutablePath';
+const CHECKED_EXECUTABLE_PATH = 'ksp.validate.checkedExecutablePath';
+const PARSER_MESSAGE_DELIMITER: string = "\t";
+const COMMAND_UNTRUST_VALIDATION_EXECUTABLE = 'ksp.untrustValidationExecutable'
+const COMMAND_UNTRUST_VALIDATION_EXECUTABLE_CONTEXT = 'ksp.untrustValidationExecutableContext'
 
 export class KSPValidationProvider
 {
-    private static MESSAGE_DELIMITER: string = "\t";
 
-    private validationEnabled: boolean = true;
-    private executable: string;
-    private pauseValidation: boolean   = false;
+    private validationEnabled: boolean          = false;
+    private realtimeValidationEnabled: boolean  = false;
+    private realtimeValidationDelay:number      = config.DEFAULT_VARIDATE_DELAY;
 
-    private documentListener: vscode.Disposable;
+    private executable: string                  = "java";
+    private pauseValidation: boolean            = false;
+    private realtimeTrigger: boolean            = false;
+
+    private onSaveListener: vscode.Disposable;
+    private onChangedListener: vscode.Disposable;
     private diagnosticCollection: vscode.DiagnosticCollection;
     private delayers: { [key: string]: ThrottledDelayer<void> };
 
@@ -35,7 +44,7 @@ export class KSPValidationProvider
     constructor( private workspaceStore: vscode.Memento )
     {
         this.delayers = Object.create( null );
-        this.documentListener = vscode.workspace.onDidSaveTextDocument( this.triggerValidate, this) ;
+        this.onSaveListener = vscode.workspace.onDidSaveTextDocument( this.triggerValidate, this) ;
     }
 
     /**
@@ -45,8 +54,10 @@ export class KSPValidationProvider
     {
         this.diagnosticCollection = vscode.languages.createDiagnosticCollection();
         subscriptions.push( this );
-        //##vscode.workspace.onDidChangeConfiguration( this.loadConfiguration, this, subscriptions );
-        //##this.loadConfiguration();
+
+        this.initConfiguration();
+        this.loadConfiguration();
+        vscode.workspace.onDidChangeConfiguration( this.loadConfiguration, this, subscriptions );
         vscode.workspace.onDidOpenTextDocument( this.triggerValidate, this, subscriptions );
         vscode.workspace.onDidCloseTextDocument( (textDocument) =>
         {
@@ -56,7 +67,7 @@ export class KSPValidationProvider
 
         subscriptions.push(
             vscode.commands.registerCommand(
-            'ksp.untrustValidationExecutable',
+            COMMAND_UNTRUST_VALIDATION_EXECUTABLE,
             this.untrustValidationExecutable, this
         ));
     }
@@ -69,23 +80,127 @@ export class KSPValidationProvider
         if( this.diagnosticCollection )
         {
             this.diagnosticCollection.clear();
-            this.diagnosticCollection.dispose();
+        }
+        this.doDispose( this.diagnosticCollection );
+    }
+
+    /**
+     * Safety dispose
+     */
+    public doDispose( p?:vscode.Disposable )
+    {
+        if( p ) p.dispose();
+    }
+
+    /**
+     * Initialize configuration.
+     * When first time of activate this extention, store default values.
+     */
+    private initConfiguration(): void
+    {
+        let section: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration( config.CONFIG_SECTION_NAME );
+        if( section )
+        {
+            let initConfig = function<T>(key:string, defaultValue:T )
+            {
+                if( !section.has( key) )
+                {
+                    section.update( key, defaultValue, true );
+                }
+            };
+            initConfig<boolean>( config.KEY_ENABLE_VARIDATE,            config.DEFAULT_ENABLE_VARIDATE );
+            initConfig<boolean>( config.KEY_ENABLE_REALTIME_VARIDATE,   config.DEFAULT_REALTIME_VARIDATE );
+            initConfig<number>(  config.KEY_REALTIME_VARIDATE_DELAY,    config.DEFAULT_VARIDATE_DELAY );
+        }
+    }
+
+    /**
+     * Load configuration for validation
+     */
+    private loadConfiguration(): void
+    {
+        let section: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration( config.CONFIG_SECTION_NAME );
+        let oldExecutable = this.executable;
+        if( section )
+        {
+            // Load implementation
+            let getConfig = function<T>(key:string, defaultValue:T, callback: ( value:T, userDefined:boolean) => void )
+            {
+                let value:T = defaultValue;
+                let userDefined: boolean = false;
+                let inspect = section.inspect<T>( key );
+                if( inspect )
+                {
+                    if( inspect.workspaceValue )
+                    {
+                        value       = inspect.workspaceValue;
+                        userDefined = true;
+                    }
+                    else if( inspect.globalValue )
+                    {
+                        value = inspect.globalValue;
+                    }
+                }
+                callback( value, userDefined );
+            };
+            // Get configurations
+            getConfig<boolean>( config.KEY_ENABLE_VARIDATE, false, (v, user) =>{
+                this.validationEnabled = v;
+            });
+            getConfig<boolean>( config.KEY_ENABLE_REALTIME_VARIDATE, false, (v, user) =>{
+                this.realtimeValidationEnabled = v;
+            });
+            getConfig<number>( config.KEY_REALTIME_VARIDATE_DELAY, config.DEFAULT_VARIDATE_DELAY, (v, user) =>{
+                if( v < 16 )
+                {
+                    this.realtimeValidationDelay = config.DEFAULT_VARIDATE_DELAY;
+                    section.update( config.KEY_REALTIME_VARIDATE_DELAY, config.DEFAULT_VARIDATE_DELAY, true );
+                    vscode.window.showWarningMessage( "KSP Configuration: " + config.KEY_REALTIME_VARIDATE_DELAY + ": too short or negative. Reset default time." );
+                }
+                else
+                {
+                    this.realtimeValidationDelay = v;
+                }
+            });
+            // ~Get configurations
+
+            if( this.pauseValidation )
+            {
+                this.pauseValidation = oldExecutable === this.executable;
+            }
+            this.doDispose( this.onChangedListener );
+            this.doDispose( this.onSaveListener );
+            this.diagnosticCollection.clear();
+
+            if( !this.validationEnabled )
+            {
+                return;
+            }
+            this.onSaveListener = vscode.workspace.onDidSaveTextDocument( (e) => {
+                let key   = e.uri.toString();
+                let delay = this.delayers[ key ];
+                if( delay )
+                {
+                    delay.cancel();
+                }
+                this.realtimeTrigger = false;
+                this.triggerValidate( e );
+            }, this );
+
+            this.onChangedListener = vscode.workspace.onDidChangeTextDocument( (e) => {
+                this.realtimeTrigger = true;
+                this.triggerValidate( e.document );
+            });
         }
     }
 
     /**
      *
      */
-    private loadConfiguration(): void
-    {}
-
-    /**
-     *
-     */
     private untrustValidationExecutable()
     {
-        this.workspaceStore.update( CheckedExecutablePath, undefined );
-        vscode.commands.executeCommand( 'setContext', 'ksp.untrustValidationExecutableContext', false );
+        this.workspaceStore.update( CHECKED_EXECUTABLE_PATH, undefined );
+        vscode.commands.executeCommand( 'setContext', COMMAND_UNTRUST_VALIDATION_EXECUTABLE_CONTEXT, false );
     }
 
     private triggerValidate( textDocument: vscode.TextDocument ): void
@@ -101,8 +216,14 @@ export class KSPValidationProvider
             let delayer = this.delayers[ key ];
             if( !delayer )
             {
-                delayer = new ThrottledDelayer<void>( 0 );
+                delayer              = new ThrottledDelayer<void>( 0 );
                 this.delayers[ key ] = delayer;
+            }
+
+            let delay = this.realtimeTrigger ? this.realtimeValidationDelay : 0;
+            if( this.realtimeValidationEnabled )
+            {
+                delayer.defaultDelay = delay;
             }
             // Exec syntax parser
             delayer.trigger( () => this.doValidate( textDocument ) );
@@ -111,7 +232,7 @@ export class KSPValidationProvider
     }
 
     /**
-     * Do syntax parser program
+     * Execute syntax parser program
      */
     private doValidate(textDocument: vscode.TextDocument): Promise<void>
     {
@@ -120,9 +241,9 @@ export class KSPValidationProvider
             let exec = "java";
             let diagnostics: vscode.Diagnostic[] = [];
 
-            let processLine = (line: string) =>
+            let processLine = (lineText: string) =>
             {
-                let msg: string[] = line.split( "\t" );
+                let msg: string[] = lineText.split( PARSER_MESSAGE_DELIMITER );
                 if( msg.length > 0 )
                 {
                     let line    = Number.parseFloat( msg[ 0 ] ) - 1; // zero origin
@@ -134,16 +255,42 @@ export class KSPValidationProvider
                     diagnostics.push( diagnostic );
                 }
             }; //~ processLine
+            let processLineStdErr = (lineText: string) =>
+            {
+                // net.rkoubou.kspparser.javacc.generated.TokenMgrError: Lexical error at line <number>,
+                let regex: RegExp = /.*?TokenMgrError\: Lexical error at line (\d+)/;
+                let matches = lineText.match( regex );
+                if( matches )
+                {
+                    let message = "FATAL Lexical error";
+                    let line = Number.parseInt( matches[ 2 ] );
+                    let diagnostic: vscode.Diagnostic = new vscode.Diagnostic(
+                        new vscode.Range( line, 0, line, Number.MAX_VALUE ),
+                        message
+                    );
+                    vscode.window.showErrorMessage( "KSPSyntaxParser.jar FATAL : Lexical error. Check your script again." );
+                }
+            }
 
             let thisExtention    = vscode.extensions.getExtension( "R-Koubou.kontakt-script-langage" );
             let thisExtentionDir = thisExtention.extensionPath;
             let options       = vscode.workspace.rootPath ? { cwd: vscode.workspace.rootPath } : undefined;
             let args: string[] = [];
+            let src = textDocument.fileName;
+            let tmpFile = undefined;
+
+            if( this.realtimeValidationEnabled )
+            {
+                tmpFile = tmp.fileSync();
+                fs.writeFileSync( tmpFile.name, textDocument.getText() );
+                src = tmpFile.name;
+            }
+
             // java -Dkspparser.datadir=path/to/data -jar kspsyntaxparser.jar <document.fileName>
             args.push( "-Dkspparser.datadir=" + thisExtentionDir + "/kspparser/data/lang/message" )
             args.push( "-jar" );
             args.push( thisExtentionDir + "/kspparser/kspsyntaxparser.jar" );
-            args.push( textDocument.fileName );
+            args.push( src );
 
             try
             {
@@ -169,9 +316,19 @@ export class KSPValidationProvider
                     {
                         processLine( data.toString() );
                     });
+                    // handling stdout
+                    childProcess.stderr.on( 'data', (data: Buffer) =>
+                    {
+                        processLineStdErr( data.toString() );
+                    });
                     // process finished
                     childProcess.stdout.on( 'end', () =>
                     {
+                        if( tmpFile )
+                        {
+                            tmpFile.removeCallback();
+                            tmpFile = undefined;
+                        }
                         this.diagnosticCollection.set( textDocument.uri, diagnostics );
                         resolve();
                     });
@@ -185,7 +342,6 @@ export class KSPValidationProvider
             {
                 this.showError( e, exec );
             }
-
         });
     }
 
@@ -198,5 +354,4 @@ export class KSPValidationProvider
         }
         vscode.window.showInformationMessage( message );
     }
-
 }
