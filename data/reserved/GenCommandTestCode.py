a842527e665e20aac3a51a69f5f7aa9fdf0ef55e
R-Koubou <dev.r.koubou@gmail.com>
2017-06-27 00:38:53 +0900

    WIP:シンボルテーブルの構築（Command） 全コールバック上でのコマンドコール生成スクリプト追加

---
+++ data/reserved/GenCommandTestCode.py
@@ -0,0 +1,92 @@
+# encoding: utf-8
+
+#
+# AnalyzerConstants.java
+# Copyright (c) R-Koubou
+#
+
+# Test statement here.
+COMMAND_STATEMENT = ""
+
+TEMPLATE = """
+
+{{ Paste to Kontakt Script Editor }}
+
+on init
+
+{{ primitive variables }}
+declare $i1
+declare $i2
+declare %ia1[ 2 ]
+declare %ia2[ 2 ]
+
+declare ~r1
+declare ~r2
+declare ?ra1[ 2 ]
+declare ?ra2[ 2 ]
+
+declare @s1
+declare @s2
+declare !sa1[ 2 ]
+declare !sa2[ 2 ]
+
+{{ ui variable }}
+declare ui_knob $Knob( 0, 1000, 1 )
+
+{testcode}
+
+end on
+
+on async_complete
+{testcode}
+end on
+
+on controller
+{testcode}
+end on
+
+on listener
+{testcode}
+end on
+
+on note
+{testcode}
+end on
+
+on persistence_changed
+{testcode}
+end on
+
+on pgs_changed
+{testcode}
+end on
+
+on poly_at
+{testcode}
+end on
+
+on release
+{testcode}
+end on
+
+on rpn
+{testcode}
+end on
+
+on nrpn
+{testcode}
+end on
+
+on ui_control( $Knob )
+{testcode}
+end on
+
+on ui_update
+{testcode}
+end on
+
+"""
+
+print( TEMPLATE.format(
+    testcode = COMMAND_STATEMENT
+))
