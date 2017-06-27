ec5b97fb120de8cd7b9242db27ceeb1401cd84b8
R-Koubou <dev.r.koubou@gmail.com>
2017-06-28 01:40:12 +0900

    WIP:シンボルテーブルの構築（Command） User Interface Commands まで対応

--- data/reserved/GenCommandTestCode.py
+++ data/reserved/GenCommandTestCode.py
@@ -7,7 +7,7 @@
 
 # Test statement here.
 COMMAND_STATEMENT = """
-
+set_ui_wf_property( $waveform, 0, 0, 0 )
 """
 
 TEMPLATE = """
@@ -17,27 +17,48 @@ TEMPLATE = """
 on init
 
 {{ primitive variables }}
-declare $i1
-declare $i2
-declare %ia1[ 2 ]
-declare %ia2[ 2 ]
+declare $i
+declare %ia[ 2 ]
 
-declare ~r1
-declare ~r2
-declare ?ra1[ 2 ]
-declare ?ra2[ 2 ]
+declare ~r
+declare ?ra[ 2 ]
 
-declare @s1
-declare @s2
-declare !sa1[ 2 ]
-declare !sa2[ 2 ]
+declare @s
+declare !sa[ 2 ]
 
 {{ ui variable }}
+declare ui_button $button
 declare ui_knob $knob( 0, 1000, 1 )
+declare ui_file_selector $selector
+declare ui_label $label(1,1)
+declare ui_level_meter $level_meter
 declare ui_menu $menu
-declare ui_label $label (1,1)
-declare ui_level_meter $level
+declare ui_slider $slider (0,100)
+declare ui_switch $switch
+declare ui_table %table[10](2,2,100)
+declare ui_text_edit @text_edit
+declare ui_value_edit $value_edit(0,100,$VALUE_EDIT_MODE_NOTE_NAMES)
 declare ui_waveform $waveform(6,6)
+declare ui_xy ?xy[4]
+
+{{
+
+ui variable names
+
+$button
+$knob
+$selector
+$label
+$level
+$menu
+$slider
+$switch
+%table[10]
+@text_edit
+$value_edit
+$waveform
+?xy
+}}
 
 {testcode}
 
