439a8ca3e4bc013a69f69e20cb00deebce6dbc1d
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-07-12 16:53:15 +0900

    Text2TSArray 整理

--- data/tools/gen.sh
+++ data/tools/gen.sh
@@ -7,8 +7,9 @@ pipenv run python spreadsheet/Excel2Snippet.py ../KSP_Snippet.xlsx ../../snippet
 
 echo  "# Generate Command Name Array : ../src/features/generated/KSPCommandNames.ts"
 pipenv run python VerifyExtractedManualData.py extract_command.txt > /dev/null
-pipenv run python Text2TSArray.py  __mergeed__.txt \
-                        ../src/features/generated/KSPCommandNames.ts \
+pipenv run python Text2TSArray.py \
+                        __mergeed__.txt \
+                        ../../src/features/generated/KSPCommandNames.ts \
                         CommandNames \
                         ui_knob \
                         ui_label \
@@ -19,10 +20,9 @@ pipenv run python Text2TSArray.py  __mergeed__.txt \
 
 echo  "# Generate Builtin Variable Name Array : ../src/features/generated/KSPBuiltinVariableNames.ts"
 pipenv run python VerifyExtractedManualData.py extract_variables.txt -v > /dev/null
-pipenv run python Text2TSArray.py  __mergeed__.txt \
-                        ../src/features/generated/KSPBuiltinVariableNames.ts \
+pipenv run python Text2TSArray.py \
+                        __mergeed__.txt \
+                        ../../src/features/generated/KSPBuiltinVariableNames.ts \
                         BuiltinVariableNames
 
 rm ./__mergeed__.txt
-
-echo TODO: ../snippets/ksp.json - remove ',' on end of last json array element
