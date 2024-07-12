c746d7395f82e6ec03090cfcd7ff640b9e55f123
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-07-12 16:53:55 +0900

    extract の出力先パスの指定漏れ対応

--- data/tools/extract.bat
+++ data/tools/extract.bat
@@ -2,10 +2,15 @@
 
 setlocal
 
-set MANUAL=KSP Reference Manual(7.10.0).txt
+set VERSION=7.10.0
+set MANUAL=KSP Reference Manual(%VERSION%).txt
 
-pipenv run python extractor\manual\ExtractCallbackFromManual.py "%MANUAL%" extract_callback.txt extract_callback_dump.txt
-pipenv run python extractor\manual\ExtractCommandFromManual.py "%MANUAL%" extract_command.txt extract_command_dump.txt
-pipenv run python extractor\manual\ExtractVariableFromManual.py "%MANUAL%" extract_variables.txt extract_variables_dump.txt
+set OUTPUT_DIR=data\extracted\%VERSION%
+
+mkdir %OUTPUT_DIR%
+
+pipenv run python extractor\manual\ExtractCallbackFromManual.py "%MANUAL%" %OUTPUT_DIR%\extract_callback.txt extract_callback_dump.txt
+pipenv run python extractor\manual\ExtractCommandFromManual.py "%MANUAL%"  %OUTPUT_DIR%\extract_command.txt extract_command_dump.txt
+pipenv run python extractor\manual\ExtractVariableFromManual.py "%MANUAL%" %OUTPUT_DIR%\extract_variables.txt extract_variables_dump.txt
 
 endlocal
--- data/tools/extract.sh
+++ data/tools/extract.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-MANUAL="KSP Reference Manual(7.10.0).txt"
+VERSION="7.10.0"
+MANUAL="data/ksp-manuals/KSP Reference Manual(${VERSION}).txt"
 
-pipenv run python extractor/manual/ExtractCallbackFromManual.py "$MANUAL" extract_callback.txt extract_callback_dump.txt
-pipenv run python extractor/manual/ExtractCommandFromManual.py "$MANUAL" extract_command.txt extract_command_dump.txt
-pipenv run python extractor/manual/ExtractVariableFromManual.py "$MANUAL" extract_variables.txt extract_variables_dump.txt
+OUTPUT_DIR="data/extracted/${VERSION}"
+
+mkdir $OUTPUT_DIR
+
+pipenv run python extractor/manual/ExtractCallbackFromManual.py "$MANUAL" $OUTPUT_DIR/extract_callback.txt extract_callback_dump.txt
+pipenv run python extractor/manual/ExtractCommandFromManual.py "$MANUAL"  $OUTPUT_DIR/extract_command.txt extract_command_dump.txt
+pipenv run python extractor/manual/ExtractVariableFromManual.py "$MANUAL" $OUTPUT_DIR/extract_variables.txt extract_variables_dump.txt
