2b148d4d56ffb7e2c43bf379c4e4f42cc76fe759
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-07-13 23:06:38 +0900

    バッチ処理修正

--- data/tools/gen.bat
+++ data/tools/gen.bat
@@ -1,13 +1,19 @@
 @echo off
 
+setlocal
+
+set LOG_DIR=.logs
+
+mkdir %LOG_DIR%\
+
 pipenv run python spreadsheet\Xlsx2Completion.py ..\KSP_Completion.xlsx ..\..\src\features\generated\
 pipenv run python spreadsheet\Xlsx2Snippet.py ..\KSP_Snippet.xlsx ..\..\snippets\
 
 call .\extract.bat
 echo "# Generate Command Name Array: ../src/features/generated/KSPCommandNames.ts"
-pipenv run python .\spreadsheet\VerifyExtractedCommandData.py ..\KSP_Completion.xlsx data\extracted\extract_command.txt __merged__.txt > nul
+pipenv run python .\spreadsheet\VerifyExtractedCommandData.py ..\KSP_Completion.xlsx data\extracted\extract_command.txt __merged_comand__.txt > %LOG_DIR%\__merged_comand_dump__.txt
 pipenv run python Text2TSArray.py ^
-    __mergeed__.txt ^
+    __merged_comand__.txt ^
     ..\..\src\features\generated\KSPCommandNames.ts CommandNames ^
     ui_knob ui_label ^
     ui_slider ^
@@ -16,10 +22,10 @@ pipenv run python Text2TSArray.py ^
     ui_waveform
 
 echo  # Generate Builtin Variable Name Array : ../src/features/generated/KSPBuiltinVariableNames.ts
-pipenv run python .\spreadsheet\VerifyExtractedVariableData.py ..\KSP_Completion.xlsx data\extracted\extract_variables.txt __merged__.txt > nul
+pipenv run python .\spreadsheet\VerifyExtractedVariableData.py ..\KSP_Completion.xlsx data\extracted\extract_variables.txt __merged_variables__.txt > %LOG_DIR%\__merged_variables_dump__.txt
 pipenv run python Text2TSArray.py ^
-    __merged__.txt ^
+    __merged_variables__.txt ^
     ..\..\srcfeatures\generated\KSPBuiltinVariableNames.ts ^
     BuiltinVariableNames
 
-del /q .\__mergeed__.txt
+endlocal
--- data/tools/gen.sh
+++ data/tools/gen.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+LOG_DIR=.logs
+
+mkdir $LOG_DIR
+
 pipenv run python spreadsheet/Xlsx2Completion.py ../KSP_Completion.xlsx ../../src/features/generated/
 pipenv run python spreadsheet/Xlsx2Snippet.py ../KSP_Snippet.xlsx ../../snippets/
 
 ./extract.sh
 
 echo  "# Generate Command Name Array : ../src/features/generated/KSPCommandNames.ts"
-pipenv run python ./spreadsheet/VerifyExtractedCommandData.py ../KSP_Completion.xlsx data/extracted/extract_command.txt __merged__.txt > /dev/null
+pipenv run python ./spreadsheet/VerifyExtractedCommandData.py ../KSP_Completion.xlsx data/extracted/extract_command.txt __merged_comand__.txt > $LOG_DIR/__merged_comand_dump__.txt
 pipenv run python Text2TSArray.py \
-                        __merged__.txt \
+                        __merged_comand__.txt \
                         ../../src/features/generated/KSPCommandNames.ts \
                         CommandNames \
                         ui_knob \
@@ -19,10 +23,8 @@ pipenv run python Text2TSArray.py \
                         ui_waveform
 
 echo  "# Generate Builtin Variable Name Array : ../src/features/generated/KSPBuiltinVariableNames.ts"
-pipenv run python ./spreadsheet/VerifyExtractedVariableData.py ../KSP_Completion.xlsx data/extracted/extract_variables.txt __merged__.txt > /dev/null
+pipenv run python ./spreadsheet/VerifyExtractedVariableData.py ../KSP_Completion.xlsx data/extracted/extract_variables.txt __merged_variables__.txt > $LOG_DIR/__merged_variables_dump__.txt
 pipenv run python Text2TSArray.py \
-                        __merged__.txt \
+                        __merged_variables__.txt \
                         ../../src/features/generated/KSPBuiltinVariableNames.ts \
                         BuiltinVariableNames
-
-rm ./__merged__.txt
